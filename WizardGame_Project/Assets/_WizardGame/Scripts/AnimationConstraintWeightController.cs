dba495f5c0981d7df613d7aa8ebb5209deb8619e
Alexander Zaikin <ay.zaikin@gmail.com>
2023-10-08 15:51:57 +0500

    Add natural head rotation

---
+++ WizardGame_Project/Assets/_WizardGame/Scripts/AnimationConstraintWeightController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class ConstraintWeightController : MonoBehaviour
+{
+    [SerializeField] private float minBackReductionAngle;
+    [SerializeField] private float maxBackReductionAngle;
+
+    [SerializeField] private Transform character;
+    [SerializeField] private Transform aim;
+    [SerializeField] private Rig rig;
+
+    private float originalWeight;
+
+    private void Start()
+    {
+        originalWeight = rig.weight;
+    }
+
+    private void Update()
+    {
+        if (character == null || aim == null || rig == null)
+            return;
+
+        var aimDirection = aim.position - character.position;
+        var backAngle = Vector3.Angle(-character.forward, aimDirection);
+        var weightScale = Mathf.Clamp01((backAngle - minBackReductionAngle) / maxBackReductionAngle);
+
+        rig.weight = originalWeight * weightScale;
+    }
+}
