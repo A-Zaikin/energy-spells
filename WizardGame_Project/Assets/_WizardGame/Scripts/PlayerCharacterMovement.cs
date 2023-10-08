5e9ebe5d6f0fa3e06b2a531a9aed07b7699fdd7c
Alexander Zaikin <ay.zaikin@gmail.com>
2023-10-08 13:23:13 +0500

    Walking animation v1

---
+++ WizardGame_Project/Assets/_WizardGame/Scripts/PlayerCharacterMovement.cs
@@ -0,0 +1,59 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace WizardGame
+{
+    public class PlayerCharacterMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private Animator animator;
+        [SerializeField, Range(0, 1)] private float directionInterpolation;
+        [SerializeField] private float acceleration;
+
+        private Vector2 input;
+        private bool hasInput;
+
+        private Vector2 movement;
+
+        private void Update()
+        {
+            input = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.W))
+                input.y += 1;
+            if (Input.GetKey(KeyCode.S))
+                input.y -= 1;
+            if (Input.GetKey(KeyCode.A))
+                input.x -= 1;
+            if (Input.GetKey(KeyCode.D))
+                input.x += 1;
+
+            hasInput = input != Vector2.zero;
+
+            if (hasInput)
+                input.Normalize();
+
+            animator.SetBool("walking", hasInput);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!hasInput || Vector2.Dot(movement, input) < 0)
+                movement = Vector2.zero;
+
+            //movement += acceleration * Time.fixedDeltaTime * input;
+            movement = Vector2.Lerp(movement, input, acceleration * Time.fixedDeltaTime);
+            if (movement.magnitude > 1)
+                movement.Normalize();
+
+            if (hasInput)
+            {
+                transform.position += speed * Time.fixedDeltaTime * movement.AsXz();
+                transform.rotation = Quaternion.Lerp(
+                    transform.rotation,
+                    Quaternion.LookRotation(movement.AsXz()),
+                    directionInterpolation);
+            }
+        }
+    }
+}
---
+++ WizardGame_Project/Assets/_WizardGame/Scripts/VectorHelper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace WizardGame
+{
+    public static class VectorHelper
+    {
+        public static Vector3 AsXz(this Vector2 vector) => new(vector.x, 0, vector.y);
+    }
+}
