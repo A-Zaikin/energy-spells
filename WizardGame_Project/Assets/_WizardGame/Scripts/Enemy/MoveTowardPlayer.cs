119f5cc477bea4fdac0d63cca47b342a9395c21d
Alexander Zaikin <ay.zaikin@gmail.com>
2023-12-07 17:52:56 +0500

    WG-3 Импортировать модель для врага ближнего боя

--- WizardGame_Project/Assets/_WizardGame/Scripts/Enemy/MoveTowardPlayer.cs
+++ WizardGame_Project/Assets/_WizardGame/Scripts/Enemy/MoveTowardPlayer.cs
@@ -17,6 +17,7 @@ namespace WizardGame
                 parameters.Get(ParameterType.Speed, out var speed))
             {
                 var direction = (Target.Current.transform.position - body.position).OnlyXz().normalized;
+                body.rotation = Quaternion.LookRotation(direction);
                 body.velocity = speed * direction;
             }
         }
